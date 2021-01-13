dd36a3ba8a947600ef01c33f2bcd75ad99de49de
Jerry Nolazco <newyork_14@live.com>
2021-01-12 21:07:45 -0700

    Parent and child classes

---
+++ classes.py
@@ -0,0 +1,21 @@
+
+
+#parent class
+class Animal:
+    #behaviors of animals    
+    legs = 'I can walk'
+    eat = 'I eat food'
+    sleep = 'I need to sleep'
+
+#child class
+class Dog(Animal):
+        #behaviors of a dog
+    play = 'I like to play fetch'
+    tailWag = 'I wag my tail when I\'m happy'
+
+
+#child class
+class Cat(Animal):
+    #behaviors of a cat
+    iFit = 'I like to fit in any tight space'
+    scratching = 'I need to groom my nails and mark territory'
