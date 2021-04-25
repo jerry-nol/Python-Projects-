7d17e645c18b0d5e8c0f7757233204f924d75096
jerry-nol <newyork_14@live.com>
2021-04-25 15:51:13 -0600

    VS
    Visual Studio basic html page

---
+++ VS/js/VS.js
@@ -0,0 +1,10 @@
+function songName() {
+    document.getElementById('song_name').innerHTML = 'Elevando Rango - Herencia De Patrones'
+}
+
+function moveElement() {
+    var moveH2 = document.getElementById('move');
+    moveH2.style.position = 'absolute';
+    moveH2.style.left = "500px";
+    moveH2.style.top = "0px"
+}
